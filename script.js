6f5d3df5e9664ee556882dd267b0dc2361b2253d
Riquelmy Oliveira <riquelmyor@gmail.com>
2023-12-15 22:10:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function atualizarValor() {
+  
+    var userInput = document.getElementById('userInput').value;
+    document.getElementById("tela_iframe").src = document.getElementById("tela_iframe").src + userInput;
+
+
+  }
+
+  
