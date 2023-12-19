68a08769353eaffb1704588dada7e16b75ccba2c
Marcello Cavalcanti de Albuquerque <marcello.albuquerque@larcopetroleo.com.br>
2023-12-19 14:17:12 -0300

    pasta css e js e ajust na index

--- script.js
+++
@@ -1,9 +0,0 @@
-function atualizarValor() {
-  
-    var userInput = document.getElementById('userInput').value;
-    document.getElementById("tela_iframe").src = document.getElementById("tela_iframe").src + userInput;
-
-
-  }
-
-  
